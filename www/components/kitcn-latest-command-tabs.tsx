560a6a41fd1226b3e2b2b53c5f41a04224becebd
zbeyens <zbeyens@udecode.dev>
2026-04-01 00:41:08 +0200

    fix

--- www/components/kitcn-latest-command-tabs.tsx
+++ www/components/kitcn-latest-command-tabs.tsx
@@ -1,5 +1,5 @@
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
-import { CodeBlock } from '@/components/code-block';
 import { getLatestKitcnCommands, packageManagers } from '@/lib/kitcn-commands';
 
 export function KitcnLatestCommandTabs({
@@ -16,9 +16,13 @@ export function KitcnLatestCommandTabs({
       {packageManagers.map((packageManager) => (
         <Tab key={packageManager} value={packageManager}>
           <CodeBlock className="my-0">
-            <code className={`language-bash package-manager-${packageManager}`}>
-              {latestCommands[packageManager]}
-            </code>
+            <Pre>
+              <code
+                className={`language-bash package-manager-${packageManager}`}
+              >
+                {latestCommands[packageManager]}
+              </code>
+            </Pre>
           </CodeBlock>
         </Tab>
       ))}
--- www/mdx-components.tsx
+++ www/mdx-components.tsx
@@ -1,14 +1,12 @@
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
-import { CodeBlock } from './components/code-block';
 import { Compare, CompareItem } from './components/compare';
 import { KitcnLatestCommandTabs } from './components/kitcn-latest-command-tabs';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    pre: (props) => <CodeBlock {...props} />,
     Tab,
     Tabs,
     Compare,
